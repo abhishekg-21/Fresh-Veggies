5ba63ed9bb0d3055fe78e2cc12d3346aabdde516
abhishekg-21 <asgdon1@example.com>
2025-12-09 12:28:51 +0530

    eee

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "randomuser.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      // Add other domains as needed
+    ],
   },
 };
 
